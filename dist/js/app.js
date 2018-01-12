3d5915bc13656919bb1dd08663ff49cb369cc54d
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-01-12 11:43:12 -0800

    add doctor look up logic which creates new promise

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,11 +2,63 @@
 exports.apiKey = "0bde8d403407d91d00e5c6a4b957fd01";
 
 },{}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var apiKey = require('./../../.env').apiKey;
+
+var doctorSearch = exports.doctorSearch = function doctorSearch() {
+  return new Promise(function (resolve, reject) {
+    var request = new XMLHttpRequest();
+    var url = "https://api.betterdoctor.com/2016-03-01/doctors?name=name&location=or-portland&skip=0&limit=10&user_key=0bde8d403407d91d00e5c6a4b957fd01";
+    request.onload = function () {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+  });
+};
+
+},{"./../../.env":1}],3:[function(require,module,exports){
 'use strict';
 
-// import {  } from './../src/js/scritps.js';
-var apiKey = require('./../.env').apiKey;
+var _doctorSearch = require('./../src/js/doctor-search.js');
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  //Testing set input
+  // $('#medical-condition-input').val('acne');
+  $('#name-input').val('name');
 
-},{"./../.env":1}]},{},[2]);
+  $('#doctor-finder').submit(function (event) {
+    event.preventDefault();
+    // const searchType = $('#medical-condition-input').val();
+    // const condition = $('#medical-condition-input').val();
+    var name = $('#name-input').val();
+    //Clear forms
+    // $('#medical-condition-input').val("");
+    // $('#name-input').val("");
+    var searchResults = doctorSerach();
+    console.log(_doctorSearch.doctorSearch);
+
+    searchResults.then(function (response) {
+      var doctors = JSON.parse(response);
+      doctors.data.map(function (doctor) {
+        if (doctors.data) {
+          $('.output').append('<div class="doctor-list-item">\n                                <h3>' + doctor.profile.first_name + ' ' + doctor.profile.last_name + ', MD</h3>\n                                <div class="address">\n                                  <span class="location"> Address: ' + doctor.practices.visit_addresss + '</span>\n                                </div>\n                                <span class="phone">Phone: ' + doctor.practices.phones.number + '</span>\n                                <span class="website">Website: ' + doctor.practices.phones.number + '</span>\n                                <span class="new-patients">Accepting New Patients: ' + doctor.practices.accepts_new_patients + '</span>\n                              </div>');
+        } else {
+          $('.output').append('There are search results for your query.');
+        }
+      });
+    }, function (error) {
+      Error('There was an error processing your request: ' + error.message);
+    });
+  });
+});
+
+},{"./../src/js/doctor-search.js":2}]},{},[3]);
--- src/js/scripts-interface.js
+++
@@ -1,9 +0,0 @@
-// import {  } from './../src/js/scritps.js';
-const apiKey = require('./../.env').apiKey;
-
-$(document).ready(function() {
-
-
-
-
-});
