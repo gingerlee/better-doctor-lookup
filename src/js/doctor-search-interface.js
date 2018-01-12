328f51e9557dfff437b9596e7ed473311d241319
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-01-12 13:21:32 -0800

    update logic function for doctor search

--- src/js/doctor-search-interface.js
+++ src/js/doctor-search-interface.js
@@ -1,4 +1,4 @@
-import { doctorSearch } from './../src/js/doctor-search.js';
+import { doctorSearch } from './../js/doctor-search.js';
 $(document).ready(function() {
 //Testing set input
   // $('#medical-condition-input').val('acne');
@@ -6,36 +6,36 @@ $(document).ready(function() {
 
   $('#doctor-finder').submit(function(event) {
     event.preventDefault();
-    // const searchType = $('#medical-condition-input').val();
-    // const condition = $('#medical-condition-input').val();
+    const searchType = $('#medical-condition-input').val();
+    const condition = $('#medical-condition-input').val();
     const name = $('#name-input').val();
 //Clear forms
     // $('#medical-condition-input').val("");
     // $('#name-input').val("");
-    const searchResults = doctorSerach();
+    const searchResults = doctorSerach(name, apiKey);
     console.log(doctorSearch);
 
-    searchResults.then(function(response) {
-    const doctors = JSON.parse(response);
-    doctors.data.map(function(doctor) {
-    if (doctors.data) {
-          $('.output').append(`<div class="doctor-list-item">
-                                <h3>${doctor.profile.first_name} ${doctor.profile.last_name}, MD</h3>
-                                <div class="address">
-                                  <span class="location"> Address: ${doctor.practices.visit_addresss}</span>
-                                </div>
-                                <span class="phone">Phone: ${doctor.practices.phones.number}</span>
-                                <span class="website">Website: ${doctor.practices.phones.number}</span>
-                                <span class="new-patients">Accepting New Patients: ${doctor.practices.accepts_new_patients}</span>
-                              </div>`
-                            );
-        } else {
-            $('.output').append(`There are search results for your query.`
-                              );
-          }
-        });
-  }, function(error) {
-      Error(`There was an error processing your request: ${error.message}`);
-    });
+  //   searchResults.then(function(response) {
+  //   const doctors = JSON.parse(response);
+  //   doctors.data.map(function(doctor) {
+  //   if (doctors.data) {
+  //         $('.output').append(`<div class="doctor-list-item">
+  //                               <h3>${doctor.profile.first_name} ${doctor.profile.last_name}, MD</h3>
+  //                               <div class="address">
+  //                                 <span class="location"> Address: ${doctor.practices.visit_addresss}</span>
+  //                               </div>
+  //                               <span class="phone">Phone: ${doctor.practices.phones.number}</span>
+  //                               <span class="website">Website: ${doctor.practices.phones.number}</span>
+  //                               <span class="new-patients">Accepting New Patients: ${doctor.practices.accepts_new_patients}</span>
+  //                             </div>`
+  //                           );
+  //       } else {
+  //           $('.output').append(`There are search results for your query.`
+  //                             );
+  //         }
+  //       });
+  // }, function(error) {
+  //     Error(`There was an error processing your request: ${error.message}`);
+  //   });
   });
 });
--- src/js/doctor-search.js
+++ src/js/doctor-search.js
@@ -1,9 +1,9 @@
 const apiKey = require('./../../.env').apiKey;
 
-export let doctorSearch = function() {
+export function doctorSearch(name, apiKey) {
   return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=name&location=or-portland&skip=0&limit=10&user_key=0bde8d403407d91d00e5c6a4b957fd01`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
